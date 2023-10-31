0669d6577ad1d980cf61f3782bcc0232a2230ea6
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-31 10:25:02 +0700

    Update code

--- commit_ids.py
+++ commit_ids.py
@@ -28,8 +28,6 @@ ids, labels, messages, codes = preprocessed_commits
 
 ic(len(ids))
 
-ic(labels[:10])
-
 # Convert the lists to sets
 set1 = set(bug_commit_ids)
 set2 = set(fix_commit_ids)
@@ -39,41 +37,41 @@ set3 = set(ids)
 common_elements = set3.intersection(set1)
 
 # Get the count of common elements
-count_common_elements = len(common_elements)
+bug_all = len(common_elements)
 
-ic(count_common_elements)
+ic(bug_all)
 
 # Find the common elements
 common_elements = set3.intersection(set2)
 
 # Get the count of common elements
-count_common_elements = len(common_elements)
+fix_all = len(common_elements)
 
-ic(count_common_elements)
+ic(fix_all)
 
 # Find the common elements
 common_elements = set1.intersection(set2)
 
 # Get the count of common elements
-count_common_elements = len(common_elements)
+bug_fix = len(common_elements)
 
-ic(count_common_elements)
+ic(bug_fix)
 
 relabels = []
 
 for index, commit_hash in enumerate(ids):
-    
     if commit_hash in bug_commit_ids:
         relabels.append('bug')
     if commit_hash in fix_commit_ids:
         relabels.append('fix')
+    if (commit_hash in bug_commit_ids) and (commit_hash in fix_commit_ids):
+        relabels.append('fix-bug')
 
-ic('bug' in relabels)
-ic('fix' in relabels)
+ic(relabels.count('bug'))
+ic(relabels.count('fix'))
+ic(relabels.count('fix-bug'))
 
 relabeled_commits = [ids, relabels, messages, codes]
-
-ic(relabels[:10])
     
 # Export non_standard_commits as a .pkl file
 with open('relabeled_commit_ids.pkl', 'wb') as file:
