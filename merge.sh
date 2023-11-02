a0e3f4bea7f6d53d9936403cc5fb43156d73f6cd
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-11-02 17:44:31 +0700

    Update bash script

--- merge.sh
+++ merge.sh
@@ -2,5 +2,5 @@
 
 python combine.py \
     -data_dir /data/gpfs/projects/punim1928/RISE/JITDP/data \
-    -project naturalness/bug-fix \
-    -file_names "repo_commits_0_preprocessed" "repo_commits_1_preprocessed" "repo_commits_2_preprocessed" "repo_commits_3_preprocessed" "repo_commits_4_preprocessed" "repo_commits_5_preprocessed" "repo_commits_6_preprocessed" "repo_commits_7_preprocessed" "repo_commits_8_preprocessed" "repo_commits_9_preprocessed" "repo_commits_10_preprocessed" "repo_commits_11_preprocessed" "repo_commits_12_preprocessed" "repo_commits_13_preprocessed" "repo_commits_14_preprocessed"
+    -project naturalness/bonus \
+    -file_names "repo_commits_0_preprocessed" "repo_commits_1_preprocessed" "repo_commits_2_preprocessed" "repo_commits_3_preprocessed" "repo_commits_4_preprocessed" "repo_commits_5_preprocessed" "repo_commits_6_preprocessed" "repo_commits_7_preprocessed"
--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 clear
 
-for i in {1..14}
+for i in {0..7}
 do
   python main.py \
     -data_dir /data/gpfs/projects/punim1928/RISE/JITDP/JIT-DP-DataCrawling/save/linux \
-    -save_dir /data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/bug-fix \
+    -save_dir /data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/bonus \
     -project linux \
     -file_name "repo_commits_$i.pkl"
 done
