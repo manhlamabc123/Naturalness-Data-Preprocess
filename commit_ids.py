6b56256c8f15da6e6cf6d2c435b3fc14e9fe1269
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-11-05 19:41:27 +0700

    Update code label dataset

--- commit_ids.py
+++ commit_ids.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 
 bug_file_name = 'bug_commit_ids.pkl'
 fix_file_name = 'fix_commit_ids.pkl'
-preprocessed_file_name = '/data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/bug-fix/linux_bug-fix.pkl'
+preprocessed_file_name = '/data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/fixed/linux_bug_fix.pkl'
 
 # Load data from the .pkl file
 with open(bug_file_name, 'rb') as file:
@@ -73,21 +73,21 @@ ic(relabels.count('normal'))
 
 relabeled_commits = [ids, relabels, messages, codes]
 
-# Split 'relabeled_commits' into three parts (e.g., first 0-10k, 10k-20k, 20k-end)
-relabeled_commits_part1 = [ids[:25000], relabels[:25000], messages[:25000], codes[:25000]]
-relabeled_commits_part2 = [ids[25001:50000], relabels[25001:50000], messages[25001:50000], codes[25001:50000]]
-relabeled_commits_part3 = [ids[500001:], relabels[500001:], messages[500001:], codes[500001:]]
+# # Split 'relabeled_commits' into three parts (e.g., first 0-10k, 10k-20k, 20k-end)
+# relabeled_commits_part1 = [ids[:25000], relabels[:25000], messages[:25000], codes[:25000]]
+# relabeled_commits_part2 = [ids[25001:50000], relabels[25001:50000], messages[25001:50000], codes[25001:50000]]
+# relabeled_commits_part3 = [ids[500001:], relabels[500001:], messages[500001:], codes[500001:]]
 
-# Export each part as a .pkl file
-with open('relabeled_commits_part1.pkl', 'wb') as file:
-    pickle.dump(relabeled_commits_part1, file)
+# # Export each part as a .pkl file
+# with open('relabeled_commits_part1.pkl', 'wb') as file:
+#     pickle.dump(relabeled_commits_part1, file)
 
-with open('relabeled_commits_part2.pkl', 'wb') as file:
-    pickle.dump(relabeled_commits_part2, file)
+# with open('relabeled_commits_part2.pkl', 'wb') as file:
+#     pickle.dump(relabeled_commits_part2, file)
 
-with open('relabeled_commits_part3.pkl', 'wb') as file:
-    pickle.dump(relabeled_commits_part3, file)
+# with open('relabeled_commits_part3.pkl', 'wb') as file:
+#     pickle.dump(relabeled_commits_part3, file)
 
 # Export non_standard_commits as a .pkl file
-with open('relabeled_commit_ids.pkl', 'wb') as file:
+with open('/data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/fixed/linux_bug_fix_relabeled.pkl', 'wb') as file:
     pickle.dump(relabeled_commits, file)
