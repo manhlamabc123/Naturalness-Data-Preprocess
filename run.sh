b87fa4bc7c2c28a3728748f09cf25c12131b74c7
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-11-09 16:39:58 +0700

    Change context

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ clear
 for i in {0..7}
 do
   python main.py \
-    -data_dir /data/gpfs/projects/punim1928/RISE/JITDP/JIT-DP-DataCrawling/save/normal_linux \
-    -save_dir /data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/context_5/normal \
+    -data_dir /data/gpfs/projects/punim1928/RISE/JITDP/JIT-DP-DataCrawling/save/bonus_linux \
+    -save_dir /data/gpfs/projects/punim1928/RISE/JITDP/data/naturalness/context_5/bonus \
     -project linux \
     -context 5 \
     -file_name "repo_commits_$i.pkl"
