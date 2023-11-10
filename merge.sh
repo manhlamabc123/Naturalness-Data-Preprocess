2bf366985b2281ffd54aac0a5988aecc886bdc5d
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-11-10 11:49:35 +0700

    Update script

--- merge.sh
+++ merge.sh
@@ -2,6 +2,6 @@
 
 python combine.py \
     -data_dir /data/gpfs/projects/punim1928/RISE/JITDP/data \
-    -project naturalness/context_5/bonus \
-    -output_file_name linux_bonus \
-    -file_names "repo_commits_0_preprocessed" "repo_commits_1_preprocessed" "repo_commits_2_preprocessed" "repo_commits_3_preprocessed" "repo_commits_4_preprocessed" "repo_commits_5_preprocessed" "repo_commits_6_preprocessed" "repo_commits_7_preprocessed"
+    -project naturalness/context_5/bug_fix \
+    -output_file_name linux_bug_fix \
+    -file_names "repo_commits_0_preprocessed" "repo_commits_1_preprocessed" "repo_commits_2_preprocessed" "repo_commits_3_preprocessed" "repo_commits_4_preprocessed" "repo_commits_5_preprocessed" "repo_commits_6_preprocessed" "repo_commits_7_preprocessed" "repo_commits_8_preprocessed" "repo_commits_9_preprocessed" "repo_commits_10_preprocessed" "repo_commits_11_preprocessed" "repo_commits_12_preprocessed" "repo_commits_13_preprocessed" "repo_commits_14_preprocessed"
